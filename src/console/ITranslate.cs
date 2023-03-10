4c3d948f4b9e9485cf943b9455fb2b9ca997ad26
psujinder <39632145+psujinder@users.noreply.github.com>
2023-03-10 11:38:42 -0500

    Delete src/console directory

--- src/console/ITranslate.cs
+++
@@ -1,7 +0,0 @@
-namespace SpeechTranslation;
-
-public interface ITranslate {
-
-    public  Task TranslateAsync();
-
-}
--- src/console/TextToSpeech.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using Microsoft.Extensions.Configuration;
-using Microsoft.CognitiveServices.Speech;
-using Microsoft.CognitiveServices.Speech.Audio;
-using System.Threading.Tasks;
